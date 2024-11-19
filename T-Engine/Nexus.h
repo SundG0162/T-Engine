0e713fdac6c3d333c53e3f22fb60c46665ed99a7
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-11-20 01:59:34 +0900

    feat : Nexus

--- T-Engine/Nexus.h
+++ T-Engine/Nexus.h
@@ -5,6 +5,27 @@ public:
 	Nexus();
 	~Nexus();
 public:
-	//int ignite(HINSTANCE )
-};
+	int ignite(HINSTANCE hInstance, LPWSTR lpCmdLine, int nCmdShow);
+	static LRESULT CALLBACK wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+private:
+    void registerWndClass();
+    void createWindow();
+    void showWindow(int nCmdShow);
+    int messageLoop();
+protected:
+    virtual void startUp() abstract;
+    virtual void mainUpdate() abstract;
+    virtual void mainRender() abstract;
+    virtual void cleanUp() abstract;
 
+public:
+    const wstring& setWindowName(const wstring& name) { _windowName = name; }
+    const HINSTANCE& getHInstance() { return _hInstance; }
+    const HWND& getHWnd() { return _hWnd; }
+protected:
+    HINSTANCE _hInstance;
+    HWND      _hWnd;
+    wstring _windowName;
+    UINT      _width;
+    UINT      _height;
+};
